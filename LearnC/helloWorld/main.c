93d8195e029ee7826352b12c52a9191358a13c21
Aranyan Rajesh <aranyan.rajesh@outlook.com>
2025-01-05 21:48:12 -0800

    Initial commit for Vex VRC 2024-25

---
+++ LearnC/helloWorld/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("H        H           IIIIIIIII\n");
+    printf("H        H               I\n");
+    printf("H        H               I\n");
+    printf("H        H               I\n");
+    printf("HHHHHHHHHH               I\n");
+    printf("H        H               I\n");
+    printf("H        H               I\n");
+    printf("H        H               I\n");
+    printf("H        H           IIIIIIIII\n");
+    return 0;
+}
---
+++ LearnC/variables/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number1 = 12;
+    int number2 = 10;
+    float iamFloat = 1.2;
+    double iamDouble = 1.14;
+    char iamChar = "A";
+    char iamString[] = "Bob";
+    int sum = number1 + number2;
+    printf("The equation %d plus %d equals %d.", number1,number2,sum);
+    return 0;
+}
